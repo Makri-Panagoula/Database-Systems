f9b8f23cadbd678d49a038de3629e2c7d84d7cad
Makri-Panagoula <79352945+Makri-Panagoula@users.noreply.github.com>
2022-12-03 14:17:35 +0200

    Solved

--- src/hp_file.c
+++ src/hp_file.c
@@ -10,41 +10,37 @@
 #define CALL_BF(call)       \
 {                           \
   BF_ErrorCode code = call; \
-  if (code != BF_OK) {         \
+  if (code != BF_OK) {      \
     BF_PrintError(code);    \
     return HP_ERROR;        \
   }                         \
 }
 
 int HP_CreateFile(char *fileName){
-  // Create file
-  CALL_BF(BF_CreateFile(fileName));
 
-  // Open file
-  int desc;
-  BF_OpenFile(fileName, &desc); 
-
-  // Create a block in the file 
   BF_Block* block;
   BF_Block_Init(&block);
 
-  // Allocate memory
-  CALL_BF(BF_AllocateBlock(desc, block)); 
+  //Create the heap file
+  CALL_BF(BF_CreateFile(fileName));
 
-  char* before = BF_Block_GetData(block);     // Get data of the first block
-  memcpy(before, "Heap", strlen("Heap")+1);   // Save some arbitrary metadata in it
+  // Create a block in the file and save in it some arbitrary metadata.
+  
+  int desc;
+  BF_OpenFile(fileName, &desc); 
+
+  CALL_BF(BF_AllocateBlock(desc, block)); 
+  char* before= BF_Block_GetData(block);
+  memcpy(before, "Heap", strlen("Heap")+1);  
 
   BF_Block_SetDirty(block); 
   CALL_BF(BF_UnpinBlock(block));
 
-  // Close file
   BF_Block_Destroy(&block); 
   CALL_BF(BF_CloseFile(desc));
-
   return 0;
 }
 
-// Κραταει και την πληροφορια οτι το αρχειο ειναι οντως αρχειο σωρου
 HP_info* HP_OpenFile(char *fileName){
   // Open file 
   int desc;
@@ -61,14 +57,16 @@ HP_info* HP_OpenFile(char *fileName){
   if(strcmp(heap,"Heap") != 0)
     return NULL;  
 
+  //Allocate memory address for the pointer we will return and initialize its fields.
   HP_info* info = malloc(sizeof(HP_info));
   info->fileDesc = desc;
-  info->bytes = 0;
 
-  // We don't need these
-  // BF_Block_Destroy(&metadata);
-  // BF_CloseFile(desc);
-  
+  //The first block holds only the metadata
+  info->records = 0;
+  //The bytes of records per block will be its total size minus the pointer part (its metadata)
+  info->tot_records = (BF_BLOCK_SIZE - sizeof(BF_Block*)) / sizeof(Record);
+
+  BF_Block_Destroy(&metadata);
   return info;
 }
 
@@ -79,51 +77,47 @@ int HP_CloseFile(HP_info* header_info ){
   return 0;
 }
 
-//Σε καθε block πρέπει να κραταμε εναν δεικτη που θα μας πηγαίνει στο επόμενο block
-//Στα μεταδεδομενα κραταμε που ακριβως θα γινει η εισαγωγη
 
 int HP_InsertEntry(HP_info* header_info, Record record){
-  int bytes_left = BF_BLOCK_SIZE - header_info->bytes - sizeof(BF_Block*);
-  int to_add = sizeof(record);
-
-  BF_Block* prev;
-  BF_Block_Init(&prev);
 
   int blocks;
+  BF_Block* prev;
+  BF_Block_Init(&prev);
   CALL_BF(BF_GetBlockCounter(header_info->fileDesc, &blocks));
-  //printf("Number of blocks: %d\n",blocks);
 
-  // Getting data from the last block 
-  CALL_BF(BF_GetBlock(header_info->fileDesc, blocks -1, prev));   
-  char* prev_data = BF_Block_GetData(prev);
+  //Getting data from the last block 
+ //Block enumeration starts from 0
 
-  // Remeber!! Block enumeration starts from 0
+  CALL_BF(BF_GetBlock(header_info->fileDesc , blocks -1  , prev));   
+  Record* prev_data = (Record*) BF_Block_GetData(prev);
 
-  // If there is not enough space to put the new record in
-  if(to_add > bytes_left || blocks == 1) {
-    // Create a new block 
+
+  if( header_info->records == header_info->tot_records  || blocks == 1) {
+
     BF_Block* new;
     BF_Block_Init(&new);
     CALL_BF(BF_AllocateBlock(header_info->fileDesc,new));
 
-    // Store pointer to new block as metadata
-    memcpy(prev_data + header_info->bytes, new, sizeof(BF_Block*));
+    //Store pointer to new block as metadata
+    void* addr = prev_data + header_info->tot_records;
+    memcpy(addr , new , sizeof(BF_Block*));
 
-    // Put record in the new block
-    char* new_block = BF_Block_GetData(new);
-    memcpy(new_block , &record , sizeof(Record));
-    header_info->bytes = sizeof(Record);  
+    //Put record in the new block
+    Record* new_block= (Record*) BF_Block_GetData(new);
+    new_block[0]=record;
+    header_info->records = 1;  
 
-    // Take care of the block
+    //Take care of the block
     BF_Block_SetDirty(new); 
     CALL_BF(BF_UnpinBlock(new));      
     BF_Block_Destroy(&new);
   }
-  // If we do not need to create a new block
+
+  //We store the record in the last block
   else {
-    // Store the record in the last available block
-    memcpy(prev_data + header_info->bytes , &record , sizeof(Record));    
-    header_info->bytes+=sizeof(Record);
+
+    prev_data[header_info->records]=record;
+    header_info->records++;
   }
 
   BF_Block_SetDirty(prev); 
@@ -137,36 +131,37 @@ int HP_GetAllEntries(HP_info* header_info, int id ){
 
   BF_Block* cur;
   BF_Block_Init(&cur);
-  printf("\ninitialization done\n");
+  printf("okay");
   int blocks;
   CALL_BF(BF_GetBlockCounter(header_info->fileDesc, &blocks));
   printf("\ncounting blocks %d\n", blocks);
 
-  int records_per_block = (BF_BLOCK_SIZE - sizeof(BF_Block*)) / sizeof(Record);
 
-  if(blocks > BF_BUFFER_SIZE) {
-    blocks = BF_BUFFER_SIZE;
-  }
+  int records = header_info->tot_records;
 
   for(int i=0; i < blocks; i++) {
+    printf("\n%d",i);
 
     //Getting data from each block 
     CALL_BF(BF_GetBlock(header_info->fileDesc , i , cur));   
-    printf("\n%d",i);
+
+    if(i == blocks - 1) {
+      records = header_info->records;
+    }
   
-    char* cur_data = BF_Block_GetData(cur);   
+    Record* data = (Record*) BF_Block_GetData(cur);   
 
-    for(int j=0; j < records_per_block; j++) {        //For each record inside the block
+    for(int j=0; j < records ; j++) {        //For each record inside the block
 
-      Record* rec= (Record*) (cur_data + j * sizeof(Record));
-
-      if(rec->id == id) {
-        printRecord(*rec);
+      if(data[j].id == id) {
+        printRecord(data[j]);
       }
     } 
+
+    CALL_BF(BF_UnpinBlock(cur));  
+
   }
 
-//  CALL_BF(BF_UnpinBlock(cur));  
   BF_Block_Destroy(&cur);
   return blocks;
 }
